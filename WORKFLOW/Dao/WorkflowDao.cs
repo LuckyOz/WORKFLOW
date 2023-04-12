42411d2bcda67e0944b9c90fce27f4fafc4e4008
fauzi pratama <fauzi.pratama@kawanlamaretail.com>
2023-04-12 17:09:02 +0700

    update

--- WORKFLOW/Dao/WorkflowDao.cs
+++ WORKFLOW/Dao/WorkflowDao.cs
@@ -6,6 +6,7 @@ namespace WORKFLOW.Dao
         Task<List<ms_workflow>> getListWorkflow();
         Task<List<ms_rule>> getListRuleByWorkflowCode(string workflowCode);
         Task<ms_rule> getRuleByWorkflowCodeAndRulesCodeforCustomAction(string workflowCode, string rulesCode);
+        Task<bool> insertDefault(List<ms_workflow> dataWorkflow, List<ms_groupworkflow> dataGroupWorkflow, List<ms_user> dataUserWorkflow);
     }
 
     public class WorkflowDao : IWorkflowDao
@@ -30,7 +31,7 @@ namespace WORKFLOW.Dao
                 .Where(q => q.workflowcode == workflowCode && q.isactive && q.enddate >= DateTime.UtcNow)
                 .Include(q => q.md_rule_vars!.OrderBy(q => q.linenum))
                 .Include(q => q.md_rule_exps!.OrderBy(q => q.linenum))
-                //.Include(q => q.md_rule_rsls!.OrderBy(q => q.linenum))
+                .Include(q => q.md_rule_rslts!.OrderBy(q => q.linenum))
                 .ToListAsync();
         }
 
@@ -39,10 +40,58 @@ namespace WORKFLOW.Dao
             ms_rule? dataRule = new ms_rule();
             dataRule = await _workflowContext.ms_rules!.AsNoTracking()
                        .Where(q => q.workflowcode == workflowCode && q.rulecode == rulesCode && q.isactive && q.enddate >= DateTime.UtcNow)
-                       //.Include(q => q.md_rule_rsls.OrderBy(q => q.linenum))
+                       .Include(q => q.md_rule_rslts!.OrderBy(q => q.linenum))
                        .FirstOrDefaultAsync();
 
             return dataRule!;
+        }
+
+        public async Task<bool> insertDefault(List<ms_workflow> listDataWorkflow, List<ms_groupworkflow> listDataGroupWorkflow, List<ms_user> listDataUserWorkflow)
+        {
+            try {
+                List<string> listWFTesting = new List<string>() {
+                    new string("PR")
+                };
+
+                var listMsWorkflowDelete = await _workflowContext.ms_workflows!.AsNoTracking()
+                    .Where(q => listWFTesting.Contains(q.workflowCode))
+                    .Include(q => q.md_workflows)
+                    .ToListAsync();
+
+                var listMsRuleDelete = await _workflowContext.ms_rules!.AsNoTracking()
+                    .Where(q => listWFTesting.Contains(q.workflowcode))
+                    .Include(q => q.md_rule_vars)
+                    .Include(q => q.md_rule_exps)
+                    .Include(q => q.md_rule_rslts)
+                    .ToListAsync();
+
+                var listMsGroupWorkflow = await _workflowContext.ms_groupworkflows!.AsNoTracking()
+                    .Where(q => listWFTesting.Contains(q.workflowcode))
+                    .Include(q => q.md_groupworkflows)
+                    .ToListAsync();
+
+                var listMsuser = await _workflowContext.ms_users!.AsNoTracking()
+                    .ToListAsync();
+
+                _workflowContext.ms_workflows!.RemoveRange(listMsWorkflowDelete);
+                _workflowContext.ms_rules!.RemoveRange(listMsRuleDelete);
+                _workflowContext.ms_groupworkflows!.RemoveRange(listMsGroupWorkflow);
+                _workflowContext.ms_users!.RemoveRange(listMsuser);
+
+                await _workflowContext.SaveChangesAsync();
+            } catch (Exception ex) { }
+
+            try {
+                await _workflowContext.ms_workflows!.AddRangeAsync(listDataWorkflow);
+                await _workflowContext.ms_groupworkflows!.AddRangeAsync(listDataGroupWorkflow);
+                await _workflowContext.ms_users!.AddRangeAsync(listDataUserWorkflow);
+
+                await _workflowContext.SaveChangesAsync();
+            } catch (Exception ex) {
+                return false;
+            }
+
+            return true;
         }
     }
 }
