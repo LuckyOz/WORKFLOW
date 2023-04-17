6628f0296e239d978be0cdcc861aa75a3c67e344
fauzi pratama <fauzi.pratama@kawanlamaretail.com>
2023-04-17 16:57:26 +0700

    update

---
+++ WORKFLOW/Model/db/tr_track.cs
@@ -0,0 +1,11 @@
+﻿namespace WORKFLOW.Model.db
+{
+    public class tr_track
+    {
+        [Key]
+        [Required]
+        [Column("documentnumber")]
+        [StringLength(100)]
+        public string? documentnumber { get; set; }
+    }
+}
